7045c11a07966ce82e3f7084ef2f41efe504f76b
GopikrishnaVadlamudi <gopi.vadlamudi10@gmail.com>
2020-04-20 23:51:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chartjs-doughnut-chart-46ezjh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/chartjs-doughnut-chart-46ezjh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit  {
+  chart: any;
+
+ngOnInit() {
+
+    this.chart = new Chart('canvas', {
+          type: 'doughnut',
+          data: {
+            labels: ['Data1','Data2'],
+            datasets: [
+              { 
+                data: [55,45],
+                backgroundColor: ['rgba(255, 0, 0, 1)','rgba(255, 0, 0, 0.1)'],
+                fill: false
+              },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            tooltips:{
+              enabled:false
+            }
+          }
+        });
+  }
+
+  toggledata(event){
+    this.chart.data.datasets[0].data = [12,15,45,7,40]
+    console.log(this.chart.data.datasets[0].data);
+    this.chart.update();
+  }
+}
